b48155a24bc3036d2c6a86ec7a2b91154071d2a1
Sakthivelan2005 <sakthivelan.shankar@gmail.com>
2026-02-28 11:52:56 +0530

    Modified Home Page for Tailor

--- app/(tailor)/Home.tsx
+++ app/(tailor)/Home.tsx
@@ -1,8 +1,21 @@
 // app/tailors.js (or .tsx)
+import { Colors } from "@/constants/theme";
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, useColorScheme, View } from "react-native";
 
 export default function TailorsScreen() {
+  const colorScheme = useColorScheme();
+  const colors = Colors[colorScheme || "light"];
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: colors.background,
+    },
+    text: { fontSize: 20, marginBottom: 10, color: colors.text },
+  });
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Tailors Section</Text>
@@ -10,8 +23,3 @@ export default function TailorsScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-  text: { fontSize: 20, marginBottom: 10 },
-});
