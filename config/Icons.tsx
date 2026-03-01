1de5a83ef6a6c6d1242c85bc8db4c02a1ac132bd
Sakthivelan2005 <sakthivelan.shankar@gmail.com>
2026-03-02 00:45:34 +0530

    Configured verified Image

--- config/Icons.tsx
+++ config/Icons.tsx
@@ -1,12 +1,12 @@
 // hooks/useThemedIcons.ts
 import { Colors } from "@/constants/theme";
-import { EvilIcons, Ionicons } from "@expo/vector-icons";
+import { AntDesign, EvilIcons, Ionicons } from "@expo/vector-icons";
 import React from "react";
 import { useColorScheme } from "react-native";
 
 export const useThemedIcons = () => {
   const colorScheme = useColorScheme();
-  const colors = Colors[colorScheme || "light"];
+  const colors = Colors["light"];
 
   return {
     back: <EvilIcons name="arrow-left" size={50} color={colors.text} />,
@@ -18,5 +18,6 @@ export const useThemedIcons = () => {
     settings: (
       <Ionicons name="settings-outline" size={28} color={colors.text} />
     ),
+    shop: <AntDesign name="shop" size={28} color={colors.text} />,
   };
 };
