dceed80aa784f2697ed4dced0d1a357a5abc09be
Sakthivelan2005 <sakthivelan.shankar@gmail.com>
2026-03-02 00:54:40 +0530

    Added ShopDetails types

---
+++ types/shopDetails.ts
@@ -0,0 +1,32 @@
+export interface ShopDetailsType {
+  // SHOP DETAILS
+  selectedSpecs: string | null;
+  setSelectedSpecs: React.Dispatch<React.SetStateAction<string | null>>;
+
+  shopName: string;
+  setShopName: (name: string) => void;
+
+  shopLocation: string;
+  setShopLocation: (location: string) => void;
+
+  houseNo: string;
+  setHouseNo: (value: string) => void;
+
+  street: string;
+  setStreet: (value: string) => void;
+
+  area: string;
+  setArea: (value: string) => void;
+
+  district: string;
+  setDistrict: (value: string) => void;
+
+  profilePhoto: string | null;
+  setProfilePhoto: (uri: string | null) => void;
+
+  shopPhoto: string | null;
+  setShopPhoto: (uri: string | null) => void;
+
+  pincode: string;
+  setPincode: (value: string) => void;
+}
