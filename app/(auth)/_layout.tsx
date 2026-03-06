5a49fcc01e3057226cd6916f6923d79841051d73
Sakthivelan2005 <sakthivelan.shankar@gmail.com>
2026-03-07 04:43:42 +0530

    Updated to layout root

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,9 +3,11 @@ import { Stack } from "expo-router";
 export default function AuthLayout() {
   return (
     <Stack>
-      <Stack.Screen name="signup" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{ headerShown: false }} />
-      <Stack.Screen name="Tailor/signup" options={{ headerShown: false }} />
+      <Stack.Screen name="LoginByPhone" options={{ headerShown: false }} />
+      <Stack.Screen name="tailor/signup" options={{ headerShown: false }} />
+      <Stack.Screen name="tailor/login" options={{ headerShown: false }} />
+      <Stack.Screen name="customer/signup" options={{ headerShown: false }} />
+      <Stack.Screen name="customer/login" options={{ headerShown: false }} />
       <Stack.Screen name="Verification" options={{ headerShown: false }} />
     </Stack>
   );
